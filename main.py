d88364f123c7940d0030fc919adb4a5046d2f667
lighthookyu <84956726+lighthookyu@users.noreply.github.com>
2022-04-18 09:39:33 +0800

    更改为云函数版本
    自行挂载到云函数即可

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
+# -*- coding: utf8 -*-
 import json
 import requests
-import re
-import execjs
-from lxml import html
 import datetime
-import sys
-import schedule
-import time
+import base64
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_pksc1_v1_5
+from Crypto.PublicKey import RSA
 
 
-def get_rsa(un, psd, lt):
-    js_res = requests.get('https://newcas.gzhu.edu.cn/cas/comm/js/des.js')
-    context = execjs.compile(js_res.text)
-    result = context.call("strEnc", un + psd + lt, '1', '2', '3')
-    return result
+def encrypt(password, public_key):
+    rsakey = RSA.importKey(public_key)
+    cipher = Cipher_pksc1_v1_5.new(rsakey)
+    cipher_text = base64.b64encode(cipher.encrypt(password.encode()))
+    return cipher_text.decode()
 
 
 class GZHU(object):
@@ -25,34 +23,11 @@ class GZHU(object):
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0',
         })
         self.url = {
-            'root': 'http://libbooking.gzhu.edu.cn/',
+            'scancode': 'http://libbooking.gzhu.edu.cn/scancode.html#/login?sta=1&sysid=1EW&lab=69&type=1',
             'user_info': 'http://libbooking.gzhu.edu.cn/ic-web/auth/userInfo',
-            'auth': 'http://libbooking.gzhu.edu.cn/ic-web/auth/address?finalAddress=http://libbooking.gzhu.edu.cn/#/ic/home&errPageUrl=http://libbooking.gzhu.edu.cn/#/error&manager=false&consoleType=16',
-            'lan_list': 'http://libbooking.gzhu.edu.cn/ic-web/Language/getLanList',
             '101': 'http://libbooking.gzhu.edu.cn/ic-web/reserve?roomIds=100647013&resvDates=20220416&sysKind=8',
             '103': 'http://libbooking.gzhu.edu.cn/ic-web/reserve?roomIds=100647014&resvDates=20220416&sysKind=8',
         }
-
-    def login(self):
-        new_cas_url = 'https://newcas.gzhu.edu.cn/cas/login'
-        res = self.client.get(new_cas_url)
-        lt = re.findall(r'name="lt" value="(.*)"', res.text)
-
-        login_form = {
-            'rsa': get_rsa(self.username, self.password, lt[0]),
-            'ul': len(self.username),
-            'pl': len(self.password),
-            'lt': lt[0],
-            'execution': 'e1s1',
-            '_eventId': 'submit',
-        }
-
-        resp = self.client.post(new_cas_url, data=login_form, )
-        selector = html.fromstring(resp.text)
-        if selector.xpath('//title/text()')[0] == '融合门户':
-            return True
-        else:
-            return False
 
     def loginLib(self, select_room):
         """
@@ -63,31 +38,37 @@ class GZHU(object):
             'Referer': 'http://libbooking.gzhu.edu.cn/',
             'Host': 'libbooking.gzhuedu.cn'
         })
-        r1 = self.client.get(self.url['auth'])
-        uuid = re.findall(r'uuid%3D(.*)%26console', r1.text)
-        self.url.update({
-            '302pages': 'http://libbooking.gzhu.edu.cn/authcenter/toLoginPage?'
-                        'redirectUrl=http%3A%2F%2Flibbooking.gzhu.edu.cn%2Fic-'
-                        'web%2F%2Fauth%2Ftoken%3Fmanager%3Dfalse%26uuid%3D{uuid}'
-                        '%26consoleType%3D16&extInfo='.format(uuid=uuid[0])
-        })
-        if self.login():
-            r2 = self.client.get(self.url['302pages'])
-            if 'Please enable it to continue.' not in r2.text:
-                with open('error.log', 'w') as fp:
-                    fp.write(r2.text)
-                print('自习室系统登录失败,最后一个请求的文本写入error.log')
-                sys.exit(2)
-            r3 = self.client.get(self.url['user_info'])
-            data = json.loads(r3.text)
-            if data['message'] == '查询成功':
-                self.client.headers.update({
-                    'token': data['data']['token']
-                })
-                print('自习室系统登录成功')
-                r4 = self.client.get(self.url[select_room])
-                room_data = json.loads(r4.text)
-                return room_data, data['data']['accNo']
+
+        # 获得publicKey
+        r1 = self.client.get('http://libbooking.gzhu.edu.cn/ic-web/login/publicKey')
+        key = json.loads(r1.text)['data']
+        publicKey = key['publicKey']
+        nonceStr = key['nonceStr']
+        psd = '{};{}'.format(self.password, nonceStr)
+        print(r1)
+
+        public_key = '-----BEGIN PUBLIC KEY-----\n' + publicKey + '\n-----END PUBLIC KEY-----'
+        password = encrypt(psd, public_key)
+        print('password:', password)
+
+        login_data = {
+           "bind": 0,
+           "logonName": self.username,
+           "password": password,
+           "type": "",
+           "unionId": ""
+        }
+        self.client.post('http://libbooking.gzhu.edu.cn/ic-web/phoneSeatReserve/login', json=login_data)
+        r3 = self.client.get(self.url['user_info'])
+        data = json.loads(r3.text)
+        if data['message'] == '查询成功':
+            self.client.headers.update({
+                'token': data['data']['token']
+            })
+            print('自习室系统登录成功')
+            r4 = self.client.get(self.url[select_room])
+            room_data = json.loads(r4.text)
+            return room_data, data['data']['accNo']
 
     def postReserve(self, acc_no, begin_time, end_time, dev_id):
         """
@@ -144,10 +125,5 @@ def start():
                       dev_id=dev_id)
 
 
-# 云函数使用这行
-# start()
-# 本地挂脚本
-schedule.every().day.at("06:16").do(start)
-while True:
-    schedule.run_pending()
-    time.sleep(5)
+def main_handler(event, context):
+    start()
